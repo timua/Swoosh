6a60bc920797bef248a4a3492f296ebbda44933c
Vu Giang <timuaxe@gmail.com>
2020-03-13 11:39:22 +0700

    Code gọi League Activity

--- app/src/main/java/com/timua/swoosh/MainActivity.kt
+++ app/src/main/java/com/timua/swoosh/MainActivity.kt
@@ -2,11 +2,18 @@ package com.timua.swoosh
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import android.content.Intent
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        getStartedBtn.setOnClickListener {
+            val leagueIntent = Intent(this, LeagueActivity::class.java)
+            startActivity(leagueIntent)
+        }
     }
 }
