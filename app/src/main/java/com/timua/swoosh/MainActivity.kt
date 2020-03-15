0c26e1458ec7c29f075247bc5c548800757a0668
Vu Giang <timuaxe@gmail.com>
2020-03-15 18:05:57 +0700

    How to log, activity lifecycle

--- app/src/main/java/com/timua/swoosh/MainActivity.kt
+++ app/src/main/java/com/timua/swoosh/MainActivity.kt
@@ -5,7 +5,7 @@ import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 import android.content.Intent
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
